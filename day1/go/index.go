09eb9df1b67bdbff98a4279ef15422d23fd510e4
Jamie Bower <j_bower@hotmail.co.uk>
2026-03-28 12:43:01 +1100

    chore: more go code day 1

--- day1/go/index.go
+++ day1/go/index.go
@@ -1,19 +1,40 @@
 package main
 
 import (
-    "fmt"
-    "os"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func check(e error) {
-    if (e != nil) {
-        panic(e)
-    }
+	if e != nil {
+		panic(e)
+	}
 }
 
 func main() {
-    var path string = "input.txt"
-    data, err := os.ReadFile(path)
-    check(err)
-    fmt.Println(data)
+	var path string = "input.txt"
+	data, err := os.ReadFile(path)
+	check(err)
+
+	var text string = string(data)
+
+	//	zeros := 0
+	//	dial := 50
+
+	var lines []string = strings.Split(strings.TrimSpace(text), "\n")
+
+	for _, line := range lines {
+		direction := string(line[0])
+		turnsStr := string(line[1:])
+		turns, err := strconv.Atoi(turnsStr)
+		check(err)
+
+		for i := 0; i < int(turns); i++ {
+
+		}
+	}
+
+	fmt.Println("hello")
 }
